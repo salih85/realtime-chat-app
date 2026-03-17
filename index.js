ab22e7526a5da5b533cb00d9540018ae050935cc
Mohammed salih.k <muhammedansaransar83@gmail.com>
2026-03-17 05:21:50 -0400

    fix error

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 require('dotenv').config();
 
-const express = require('express')
-const app =express()
+const express = require('express');
+const app = express();
+const path = require('path');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
-const connectDB =require('./config/db')
+const connectDB = require('./config/db');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'static')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+const http = require('http');
+const { Server } = require('socket.io');
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
 
 
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log("server started at port",port);
+server.listen(port, () => {
+  console.log("server started at port " + port);
   connectDB();
 });
