cbada7f08ee082be6261d645eee23844fb2b52eb
Mohammed salih.k <muhammedansaransar83@gmail.com>
2026-03-18 13:40:13 -0400

    fix route

--- routes/chat.js
+++ routes/chat.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { getUsers, getMessages } = require('../controllers/chatController');
 const { protect } = require('../middlewears/authMiddleware');
 
+
 router.get('/users', protect, getUsers);
 router.get('/messages/:userId', protect, getMessages);
 
